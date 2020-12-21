6ed905c084676238d18f120ce35a79e083d10ba4
Connor Macdonald <csm119@uclive.ac.nz>
2020-12-21 22:07:08 +1300

    Added vga_text driver. - Simple driver for writing to the vga buffer. Implementation is thread and memory-safe. Only unsafe code is the definition of the vga buffer memory region. - Added print! and println! macros to write to the vga buffer - Added printing of the panic message on kernel panic.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 #![no_std] // Cannot use std library due to OS hooks
 #![no_main] // Since the std library is not included. We must self-define the start point
 use core::panic::PanicInfo;
+mod vga_text;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static MESSAGE: &[u8] = b"Hello World!";
 
 // Entry point for kernel
 #[no_mangle] // Forces function symbol to be called _start in linker
 pub extern "C" fn _start() -> ! {
-
-    // Write Hello World to vga buffer. This method is written as unsafe as a quick
-    // test of our bootloader but can/will be written in a safe context in future.
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in MESSAGE.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    panic!("Hello World{}", "!");
 
     loop {}
 }
