5588a0656c6ea8bc1811f6238116b94eb3ce4d07
Connor Macdonald <csm119@uclive.ac.nz>
2020-12-24 00:25:38 +1300

    Added memory heap and rust allocator support. - Added memory heap using a page mapping to usable memory regions. - Added a custom allocator and error handler for rust. This allows us to use many dynamic memory core structures such as vectors and boxes.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use abacus_os::println;
+use abacus_os::{println, memory, allocator};
 use bootloader::{BootInfo, entry_point};
-use alloc::boxed::Box;
+
+extern crate alloc;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use x86_64::VirtAddr;
+use abacus_os::memory::BootInfoFrameAllocator;
 
 entry_point!(kernel_main);
 
@@ -17,8 +21,33 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     abacus_os::init();
 
-    // Testing dummy allocator
-    let x = Box::new(41);
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
+    // […] call `test_main` in test context
 
     #[cfg(test)]
         test_main();
