3f774cf0c15b53ea106cee3a712ff32c5e5f6416
Connor Macdonald <csm119@uclive.ac.nz>
2020-12-25 22:32:55 +1300

    Added Multiple Allocator Architectures. - Added a bump, linked_list and fixed size block allocator. - These were added mainly as practice and educational purposes. For right now the linked list is set as the default kernel allocator. - All the allocators are in the most basic form and have a lot of room for optimisation. Particularly the linked_list could be improved by merging free space blocks, this however would require implementing a sorted list of all free space slots.

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,15 +1,18 @@
-use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
+pub mod bump;
+pub mod linked_list;
+pub mod fixed_size_block;
+
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
     },
     VirtAddr,
 };
-use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<LinkedListAllocator> =
+    Locked::new(LinkedListAllocator::new());
 
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
@@ -42,4 +45,29 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
+/// Which is always true for GlobalAlloc
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
